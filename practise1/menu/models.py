f537797e80abe39849663255ca8c8c8c6e25519f
faizan619 <alamf6023@gmail.com>
2025-09-10 08:47:42 +0530

    done

--- practise1/menu/models.py
+++ practise1/menu/models.py
@@ -3,4 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Menu(models.Model):
-    https://docs.djangoproject.com/en/5.2/topics/db/models/
+    # https://docs.djangoproject.com/en/5.2/topics/db/models/
+    # https://docs.djangoproject.com/en/5.2/ref/templates/builtins/
+
+    # servicesData = Services.objects.all().order_by("-services_icons")[:3]
+    # serviceDetail = Services.objects.get(id=serviceid)    ## get specific Details with ID
+    # for n in servicesData:
+    #     print(n.services_icons)
